649534e1e6e7e7438b72fc7b1a9590a6dd28c580
Joako55 <Garbarian5@gmail.com>
2017-01-26 11:31:11 -0300

    Facebook Update 26/01/2017
    app2 creada
    langs,locallib,settings y version update

--- lang/es/local_facebook.php
+++ lang/es/local_facebook.php
@@ -21,6 +21,7 @@
  * @package    local
  * @subpackage facebook
  * @copyright  2013 Francisco García Ralph (francisco.garcia.ralph@gmail.com)
+ * @copyright  2017 Joaquin Rivano (jrivano@alumnos.uai.cl)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 $string['messageprovider:notification'] = 'Notificaciones de recursos (pdf, excel, word, url, etc)';
@@ -88,3 +89,58 @@ $string["notificationcountA"] = "Tienes ";
 $string["notificationcountsingular"] = " nueva notificación de WebCursos";
 $string["notificationcountplural"] = " nuevas notificaciones de WebCursos";
 $string["tasks_facebook"]="Envío de notificaciones a Facebook";
+$string['sunday']="Domingo";
+$string['monday']="Lunes";
+$string['tuesday']="Martes";
+$string['wednesday']="Miercoles";
+$string['thursday']="Jueves";
+$string['friday']="Viernes";
+$string['saturday']="Sabado";
+$string['january']="enero";
+$string['february']="febrero";
+$string['march']="marzo";
+$string['april']="abril";
+$string['may']="mayo";
+$string['june']="junio";
+$string['july']="julio";
+$string['august']="agosto";
+$string['september']="septiembre";
+$string['october']="octubre";
+$string['november']="noviembre";
+$string['december']="diciembre";
+$string['of']=" de ";
+$string['from']=" del ";
+
+//Lang settings paneles
+$string['card1icon']="Icono primer panel";
+$string['card1icondesc']="Aqui se define el icono que se mostrara en el primer panel, para encontrar mas opciones de iconos ingrese a https://material.io/icons/";
+$string['card1title']="Titulo Primer panel";
+$string['card1titledesc']="Aqui se define cual sera el titulo del primer panel";
+$string['card1text']="Texto primer panel";
+$string['card1textdesc']="Aqui se define cual sera el texto dentro del primer panel";
+$string['card1link']="link de mas informacion primer link";
+$string['card1linkdesc']="Aqui se define cual sera el link que redirigira el primer panel";
+$string['card2icon']="Icono segundo panel";
+$string['card2icondesc']="Aqui se define el icono que se mostrara en el segundo panel, para encontrar mas opciones de iconos ingrese a https://material.io/icons/";
+$string['card2title']="Titulo segundo panel";
+$string['card2titledesc']="Aqui se define cual sera el titulo del segundo panel";
+$string['card2text']="Texto segundo panel";
+$string['card2textdesc']="Aqui se define cual sera el texto dentro del segundo panel";
+$string['card2link']="link de mas informacion segundo link";
+$string['card2linkdesc']="Aqui se define cual sera el link que redirigira el segundo panel";
+$string['card3icon']="Icono tercer panel";
+$string['card3icondesc']="Aqui se define el icono que se mostrara en el tercer panel, para encontrar mas opciones de iconos ingrese a https://material.io/icons/";
+$string['card3title']="Titulo tercer panel";
+$string['card3titledesc']="Aqui se define cual sera el titulo del tercer panel";
+$string['card3text']="Texto tercer panel";
+$string['card3textdesc']="Aqui se define cual sera el texto dentro del tercer panel";
+$string['card3link']="link de mas informacion tercer link";
+$string['card3linkdesc']="Aqui se define cual sera el link que redirigira el tercer panel";
+$string['card4icon']="Icono cuarto panel";
+$string['card4icondesc']="Aqui se define el icono que se mostrara en el cuarto panel, para encontrar mas opciones de iconos ingrese a https://material.io/icons/";
+$string['card4title']="Titulo cuarto panel";
+$string['card4titledesc']="Aqui se define cual sera el titulo del cuarto panel";
+$string['card4text']="Texto cuarto panel";
+$string['card4textdesc']="Aqui se define cual sera el texto dentro del cuarto panel";
+$string['card4link']="link de mas informacion cuarto link";
+$string['card4linkdesc']="Aqui se define cual sera el link que redirigira el cuarto panel";
