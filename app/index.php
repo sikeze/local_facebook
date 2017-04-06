cf5d36cc41d0d10c6a8a3aafba6e8aad0096baf4
Mark Michaelsen <mmichaelsen678@gmail.com>
2017-04-06 18:05:29 -0300

    var dump

--- app/index.php
+++ app/index.php
@@ -192,6 +192,7 @@ if ($userfacebookinfo != false) {
 		$courseid = $courses->id;
 		$shortname = $courses->shortname;
 		$totals = $courses->totalnotifications;
+		var_dump($totals);
 		
 		echo '<div class="block" style="height: 4em;"><button type="button" class="btn btn-info btn-lg" style="white-space: normal; width: 90%; height: 90%; border: 1px solid lightgray; background: #F0F0F0;" courseid="' . $courseid . '" fullname="' . $fullname . '" moodleid="'.$moodleid.'" lastvisit="'.$lastvisit.'" component="button">';
 		echo '<p class="name" align="left" style="position: relative; height: 3em; overflow: hidden; color: black; font-weight: bold; text-decoration: none; font-size:13px; word-wrap: initial;" courseid="' . $courseid . '" moodleid="'.$moodleid.'" lastvisit="'.$lastvisit.'" component="button">
